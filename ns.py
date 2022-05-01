ffb9497442934567db13a12d1a0954cd0f37b6e2
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2022-04-30 22:09:34 -0700

    ns: Rewrite to use Python's proper XML parsing.

--- ns.py
+++ ns.py
@@ -1,34 +1,28 @@
 import argparse
 import re
-import xmlParsing
+import xml.dom.pulldom
+
+import pulldomHelpers
 
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('input_path')
 	parser.add_argument('output_path')
 	parser.add_argument('namespace', type=int, help='The index of the namespace to filter out.')
-	parser.add_argument('-v', '--verbose', action='store_true', help='Print occasional progress updates.')
 	args = parser.parse_args()
-	# we only use this as a str, but it should have the form of an int
-	args.namespace = str(args.namespace)
 
-	with open(args.input_path) as inFile:
-		with open(args.output_path, 'w') as outFile:
-			# just for verbose printing
-			i = 0
-			print('<mediawiki>', file=outFile)
-			prePageLines, pageStartMatch = xmlParsing.readUntil(inFile, xmlParsing.PAGE_START)
-			while pageStartMatch:
-				nsLines, nsMatch = xmlParsing.readUntil(inFile, xmlParsing.NS_PATTERN)
-				if nsMatch[1] == args.namespace:
-					pageEndLines = xmlParsing.readUntil(inFile, xmlParsing.PAGE_END)[0]
-					outFile.write(prePageLines[-1])
-					outFile.writelines(nsLines + pageEndLines)
-				prePageLines, pageStartMatch = xmlParsing.readUntil(inFile, xmlParsing.PAGE_START)
-				if args.verbose and i % 10000 == 0:
-					print(i)
-				i += 1
-			print('</mediawiki>', file=outFile)
+	doc = xml.dom.pulldom.parse(args.input_path)
+	with open(args.output_path, 'w') as outFile:
+		outFile.write('<mediawiki>')
+		for event, node in doc:
+			if event == xml.dom.pulldom.START_ELEMENT and node.tagName == 'page':
+				doc.expandNode(node)
+				ns = int(pulldomHelpers.getDescendantContent(node, 'ns'))
+				if ns == args.namespace:
+					outFile.write('\n  ')
+					node.writexml(outFile)
+
+		outFile.write('\n</mediawiki>\n')
 
 if __name__ == '__main__':
 	main()
