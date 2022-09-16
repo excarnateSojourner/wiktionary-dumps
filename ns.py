e31408e77cfb580b4f962a482bcdfd1d3ed9a711
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2022-09-15 19:46:29 -0700

    ns: -v: Add.

--- ns.py
+++ ns.py
@@ -4,14 +4,18 @@ import xml.dom.pulldom
 
 import pulldomHelpers
 
+VERBOSE_FACTOR = 10 ** 4
+
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('input_path')
 	parser.add_argument('output_path')
 	parser.add_argument('namespace', type=int, help='The index of the namespace to filter out.')
+	parser.add_argument('-v', '--verbose', action='store_true')
 	args = parser.parse_args()
 
 	doc = xml.dom.pulldom.parse(args.input_path)
+	count = 0
 	with open(args.output_path, 'w') as outFile:
 		outFile.write('<mediawiki>')
 		for event, node in doc:
@@ -21,6 +25,9 @@ def main():
 				if ns == args.namespace:
 					outFile.write('\n  ')
 					node.writexml(outFile)
+					if args.verbose and count % VERBOSE_FACTOR == 0:
+						print(count)
+					count += 1
 
 		outFile.write('\n</mediawiki>\n')
 
