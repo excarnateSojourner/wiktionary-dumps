0399050b2a93d29c72d7b345747d0a0947aa5392
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2023-05-05 12:58:30 -0700

    pulldomHelpers: Extract descendants of a <page>.
    In pulldomHelpers.py, add a function that extracts the text of
    descendants of a <page> tag (e.g. <title> and <id>) without expanding
    the entire <page>. Also add type hints.

--- pulldomHelpers.py
+++ pulldomHelpers.py
@@ -1,12 +1,29 @@
-def getDescendantContent(node, childName):
+from typing import Iterable, Optional
+import xml.dom.minidom
+import xml.dom.pulldom
+
+def getDescendantContent(node: xml.dom.minidom.Element, childName: str) -> Optional[str]:
 	try:
 		return getText(node.getElementsByTagName(childName)[0])
 	except StopIteration:
 		return None
 
-def getText(node):
+def getText(node: xml.dom.minidom.Element) -> str:
 	if node.hasChildNodes():
 		node.normalize()
 		return node.firstChild.data
 	else:
 		return ''
+
+def getPageDescendantText(path: str, tags: list[str]) -> Iterable[dict[str, str]]:
+	doc = xml.dom.pulldom.parse(path)
+	try:
+		while True:
+			values = {}
+			for tag in tags:
+				node = next(node for event, node in doc if event == xml.dom.pulldom.START_ELEMENT and node.tagName == tag)
+				doc.expandNode(node)
+				values[tag] = getText(node)
+			yield values
+	except StopIteration:
+		return
