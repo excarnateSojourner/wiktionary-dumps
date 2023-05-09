91e6110ad4b54af8adfdd0d26cc5639648caa7b1
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2023-05-09 12:29:17 -0700

    deepCatFilter: Handle redirects to form-of temps.
    When a template is a redirect to a form-of template, treat it as a
    form-of template.
    Consistently use snake_case for variable names.
    Improve the argparse help.

--- deepCatFilter.py
+++ deepCatFilter.py
@@ -1,15 +1,17 @@
 import argparse
 import collections
-from typing import Iterator, Optional, Union
+from typing import Optional, Union
 import xml.dom.pulldom
 
 import wikitextparser
 
 import parseCats
+import parseRedirects
 import pulldomHelpers
 
 PAGES_VERBOSITY_FACTOR = 10 ** 4
 CAT_PREFIX = 'Category:'
+TEMP_PREFIX = 'Template:'
 # The id of Category:Form-of templates
 FORM_OF_TEMP_CAT_ID = 3991887
 LABEL_TEMPS = {'label', 'lb', 'lbl'}
@@ -23,115 +25,125 @@ def main():
 	parser.add_argument('-n', '--input-ids', action='store_true', help='Indicates that the categories to include and exclude are specified using their ids rather than their names. Specifying ids removes the need for this program to perform time-intensive name-to-id translation.')
 	parser.add_argument('-u', '--output-ids', action='store_true', help='Indicates that the output should be given as a list of IDs rather than a list of terms. Ignored if --pages-path is given (indicating that the text of entries should be processed as well).')
 	parser.add_argument('-p', '--pages-path', help='Only intended for mainspace Wiktionary entries. If given, an additional layer of filtering is performed to remove forms of terms that are removed by analyzing their sense lines. This is useful because on Wiktionary forms (e.g. inflections and alternative forms) often lack the full categorization of their lemmas.')
+	parser.add_argument('-r', '--redirects-path', help='The path of the CSV redirects file produced by parseRedirects.py. Ignored if --pages-path is not given.')
 	parser.add_argument('-t', '--temps-cache-path', help='The path of a file in which to cache (and later retrieve) a list of templates required for form-of filtering (triggered by --pages-path).')
-	parser.add_argument('-l', '--label-lang', help='The ISO 639 code of the language for which to exclude labels. Has no effect unless --exclude-labels is also given.')
+	parser.add_argument('-l', '--label-lang', help='The Wiktionary language code (usually the ISO 639 code) of the language for which to exclude labels. Ignored if --exclude-labels is not also given.')
 	parser.add_argument('-x', '--exclude-labels', nargs='+', default=[], help='Labels are positional arguments of the {{label}} (AKA {{lb}}) template (excluding the language code). Requires --label-lang.')
-	parser.add_argument('-m', '--exclude-temps', nargs='+', default=[], help='Templates that should cause terms to be exluded.')
+	parser.add_argument('-m', '--exclude-temps', nargs='+', default=[], help='If a given template given here is used in a sense of a term, that sense will not support the inclusion of the term. (The term may still be included if it has other "valid" senses.)')
 	parser.add_argument('-v', '--verbose', action='store_true')
 	args = parser.parse_args()
 
 	if args.input_ids:
-		includeCats = set(int(i) for i in args.include)
-		excludeCats = set(int(i) for i in args.exclude)
+		include_cats = set(int(i) for i in args.include)
+		exclude_cats = set(int(i) for i in args.exclude)
 	else:
 		if args.verbose:
 			print('Translating category names to ids...')
-		initialIncludes = set(c.removeprefix(CAT_PREFIX) for c in args.include)
-		initialExcludes = set(c.removeprefix(CAT_PREFIX) for c in args.exclude)
-		includeCats = set()
-		excludeCats = set()
+		initial_includes = set(c.removeprefix(CAT_PREFIX) for c in args.include)
+		initial_excludes = set(c.removeprefix(CAT_PREFIX) for c in args.exclude)
+		include_cats = set()
+		exclude_cats = set()
 		for data in parseCats.catsGen(args.categories_path):
-			if data.catTitle in initialIncludes:
-				includeCats.add(data.catId)
-				initialIncludes.remove(data.catTitle)
-			if data.catTitle in initialExcludes:
-				excludeCats.add(data.catId)
-				initialExcludes.remove(data.catTitle)
-		if initialIncludes or initialExcludes:
+			if data.catTitle in initial_includes:
+				include_cats.add(data.catId)
+				initial_includes.remove(data.catTitle)
+			if data.catTitle in initial_excludes:
+				exclude_cats.add(data.catId)
+				initial_excludes.remove(data.catTitle)
+		if initial_includes or initial_excludes:
 			print('I was unable to find any pages in the following categories. This either means they do not exist (check your spelling) or they are empty.')
-			print(', '.join(initialIncludes | initialExcludes))
+			print(', '.join(initial_includes | initial_excludes))
 		print('If you want to include and exclude the same sets of categories later, and you want to provide their IDs instead of titles (so I do not have to translate to IDs for you), here are the IDs formatted as command-line arguments:')
-		print('-i ' + ' '.join(str(i) for i in includeCats) + ' -e ' + ' '.join(str(e) for e in excludeCats) + ' -n')
+		print('-i ' + ' '.join(str(i) for i in include_cats) + ' -e ' + ' '.join(str(e) for e in exclude_cats) + ' -n')
 
-	terms = catFilter(args.categories_path, includeCats, excludeCats, returnTitles=not args.output_ids and not args.pages_path, verbose=args.verbose)
+	terms = catFilter(args.categories_path, include_cats, exclude_cats, return_titles=not args.output_ids or args.pages_path, verbose=args.verbose)
 	if args.pages_path:
-		terms = entryTextFilter(terms, args.categories_path, args.pages_path, args.label_lang, args.exclude_labels, args.exclude_temps, args.temps_cache_path, verbose=args.verbose)
+		terms = entryTextFilter(terms, args.categories_path, args.pages_path, args.redirects_path, args.temps_cache_path, args.label_lang, args.exclude_labels, args.exclude_temps, verbose=args.verbose)
 
-	with open(args.output_path, 'w', encoding='utf-8') as outFile:
+	with open(args.output_path, 'w', encoding='utf-8') as out_file:
 		for term in terms:
-			print(term, file=outFile)
+			print(term, file=out_file)
 
-def catFilter(categories_path: str, includeCats: set[int], excludeCats: set[int], returnTitles: bool = False, verbose: bool = False) -> Union[set[int], set[str]]:
+def catFilter(categories_path: str, include_cats: set[int], exclude_cats: set[int], return_titles: bool = False, verbose: bool = False) -> Union[set[int], set[str]]:
 	if verbose:
 		print('Looking for pages and subcategories in specified categories:')
 	# collect subcats to process in the next round
-	nextIncludeCats = set()
-	nextExcludeCats = set()
+	next_include_cats = set()
+	next_exclude_cats = set()
 	# collect non-cat pages in cats
-	includePages = set()
-	excludePages = set()
+	include_pages = set()
+	exclude_pages = set()
 
 	depth = 0
-	while includeCats or excludeCats:
+	while include_cats or exclude_cats:
 		if verbose:
 			print('', '-' * 10, f'Round {depth}', '-' * 10, sep='\n')
 		for data in parseCats.catsGen(categories_path):
-			if data.catId in includeCats:
+			if data.catId in include_cats:
 				if data.pageTitle.startswith(CAT_PREFIX):
-					nextIncludeCats.add(data.pageId)
+					next_include_cats.add(data.pageId)
 					if verbose:
 						print(f'including "{data.pageTitle.removeprefix(CAT_PREFIX)}"')
-				elif returnTitles:
-					includePages.add(data.pageTitle)
+				# a page to include
+				elif return_titles:
+					include_pages.add(data.pageTitle)
 				else:
-					includePages.add(data.pageId)
-			if data.catId in excludeCats:
+					include_pages.add(data.pageId)
+			if data.catId in exclude_cats:
 				if data.pageTitle.startswith(CAT_PREFIX):
-					nextExcludeCats.add(data.pageId)
+					next_exclude_cats.add(data.pageId)
 					if verbose:
 						print(f'excluding "{data.pageTitle.removeprefix(CAT_PREFIX)}"')
-				elif returnTitles:
-					excludePages.add(data.pageTitle)
+				# a page to exclude
+				elif return_titles:
+					exclude_pages.add(data.pageTitle)
 				else:
-					excludePages.add(data.pageId)
-		includeCats = nextIncludeCats
-		excludeCats = nextExcludeCats
-		nextIncludeCats = set()
-		nextExcludeCats = set()
+					exclude_pages.add(data.pageId)
+		include_cats = next_include_cats
+		exclude_cats = next_exclude_cats
+		next_include_cats = set()
+		next_exclude_cats = set()
 		depth += 1
 
-	return includePages - excludePages
+	return include_pages - exclude_pages
 
-def entryTextFilter(terms: set[str], categories_path: str, pages_path: str, label_lang: str, exclude_labels: set[str], exclude_temps: set[str], temps_cache_path: Optional[str] = None, verbose: bool = False) -> set[str]:
+def entryTextFilter(terms: set[str], categories_path: str, pages_path: str, redirects_path: str, temps_cache_path: Optional[str], label_lang: str, exclude_labels: set[str], exclude_temps: set[str], verbose: bool = False) -> set[str]:
 
+	if verbose:
+		print('Finding all form-of templates and their aliases...')
 	if temps_cache_path:
 		try:
-			with open(temps_cache_path, encoding='utf-8') as tempsCacheFile:
-				formOfTemps = set(tempsCacheFile.read().splitlines())
+			with open(temps_cache_path, encoding='utf-8') as temps_cache_file:
+				form_of_temps = set(temps_cache_file.read().splitlines())
 		except FileNotFoundError:
-			formOfTemps = findFormOfTemps(categories_path)
-			with open(temps_cache_path, 'w', encoding='utf-8') as tempsCacheFile:
-				for temp in formOfTemps:
-					print(temp, file=tempsCacheFile)
+			form_of_temps = findFormOfTemps(categories_path, redirects_path)
+			with open(temps_cache_path, 'w', encoding='utf-8') as temps_cache_file:
+				for temp in form_of_temps:
+					print(temp, file=temps_cache_file)
 	else:
-		formOfTemps = findFormOfTemps(categories_path)
+		form_of_temps = findFormOfTemps(categories_path, redirects_path)
 
-	senseLines = findSenseLines(terms, pages_path, verbose)
+	sense_lines = findSenseLines(terms, pages_path, verbose)
 
 	if verbose:
 		print('Checking forms...')
-	for term in senseLines:
-		if not checkTerm(term, senseLines, formOfTemps, excludeLabels, excludeTemps):
+	for term in sense_lines:
+		if not checkTerm(term, sense_lines, form_of_temps, exclude_labels, exclude_temps):
 			terms.remove(term)
 
 	return terms
 
-def findFormOfTemps(categories_path: str) -> set[str]:
-	return {temp.removeprefix('Template:') for temp in catFilter(categories_path, {FORM_OF_TEMP_CAT_ID}, set(), returnTitles=True)}
+def findFormOfTemps(categories_path: str, redirects_path: str) -> set[str]:
+	form_of_temps = {temp.removeprefix(TEMP_PREFIX) for temp in catFilter(categories_path, {FORM_OF_TEMP_CAT_ID}, set(), return_titles=True)}
+	# assumes no double redirects to form-of templates exist
+	for red in parseRedirects.redirectsGen(redirects_path):
+		if red.dstTitle.startswith(TEMP_PREFIX) and red.dstTitle.removeprefix(TEMP_PREFIX) in form_of_temps:
+			form_of_temps.add(red.srcTitle.removeprefix(TEMP_PREFIX))
+	return form_of_temps
 
 def findSenseLines(terms: set[str], pages_path: str, verbose: bool = False) -> dict[str, list[str]]:
 	doc = xml.dom.pulldom.parse(pages_path)
-	senseLines = {}
+	sense_lines = {}
 
 	if verbose:
 		print('\nLoading pages data:')
@@ -141,34 +153,36 @@ def findSenseLines(terms: set[str], pages_path: str, verbose: bool = False) -> d
 			doc.expandNode(node)
 			term = pulldomHelpers.getText(node)
 			if term in terms:
-				textNode = next(n for e, n in doc if e == xml.dom.pulldom.START_ELEMENT and n.tagName == 'text')
-				doc.expandNode(textNode)
-				senseLines[term] = [li for li in pulldomHelpers.getText(textNode).splitlines() if li.startswith('# ')]
+				text_node = next(n for e, n in doc if e == xml.dom.pulldom.START_ELEMENT and n.tagName == 'text')
+				doc.expandNode(text_node)
+				sense_lines[term] = [li for li in pulldomHelpers.getText(text_node).splitlines() if li.startswith('# ')]
 
 			if verbose and count % PAGES_VERBOSITY_FACTOR == 0:
 				print(count)
 			count += 1
-	return senseLines
+	return sense_lines
 
-def checkTerm(term: set[str], senseLines: dict[str, list[str]], formOfTemps: set[str], excludeLabels: set[str], excludeTemps: set[str]) -> bool:
-	print(f'DEBUG: checking {term}')
-	for line in senseLines[term]:
+def checkTerm(term: set[str], sense_lines: dict[str, list[str]], form_of_temps: set[str], exclude_labels: set[str], exclude_temps: set[str], time_to_live: int = 4) -> bool:
+	# if after following a few links we still haven't found a lemma, assume we are in a loop and accept the term
+	if time_to_live <= 0:
+		return True
+	for line in sense_lines[term]:
 		temps = wikitextparser.parse(line).templates
 		try:
-			formOfTemp = next(t for t in temps if t.normal_name() in formOfTemps)
-			mainForm = (formOfTemp.get_arg('2') or formOfTemp.get_arg('1')).value
-			if mainForm not in senseLines or not checkTerm(mainForm, senseLines, formOfTemps, excludeLabels, excludeTemps):
+			form_of_temp = next(t for t in temps if t.normal_name() in form_of_temps)
+			term = (form_of_temp.get_arg('2') or form_of_temp.get_arg('1')).value
+			if term not in sense_lines or not checkTerm(term, sense_lines, form_of_temps, exclude_labels, exclude_temps, time_to_live - 1):
 				continue
 		except StopIteration:
 			pass
 		try:
-			labelTemp = next(t for t in temps if t.normal_name() in LABEL_TEMPS and t.arguments[0].positional and t.get_arg('1').value == 'en')
-			labels = {a.value for a in labelTemp.arguments[1:] if a.positional}
-			if not labels.isdisjoint(excludeLabels):
+			label_temp = next(t for t in temps if t.normal_name() in LABEL_TEMPS and t.arguments[0].positional and t.get_arg('1').value == 'en')
+			labels = {a.value for a in label_temp.arguments[1:] if a.positional}
+			if not labels.isdisjoint(exclude_labels):
 				continue
 		except StopIteration:
 			pass
-		if any(temp.normal_name() in excludeTemps for temp in temps):
+		if any(temp.normal_name() in exclude_temps for temp in temps):
 			continue
 		return True
 	return False
