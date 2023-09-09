0eca39db5aad4008058b14e5defa5bc0dcbbbdf0
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2023-09-09 13:05:58 -0700

    ns: Filter out multiple namespaces simultaneously.
    In ns.py allow the specification of multiple namespaces. All of these
    are copied out of the input pages file in the same pass.

--- ns.py
+++ ns.py
@@ -9,27 +9,32 @@ VERBOSE_FACTOR = 10 ** 4
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('input_path')
-	parser.add_argument('output_path')
 	parser.add_argument('namespaces', nargs='+', type=int, help='The index(es) of the namespace(s) to select.')
+	parser.add_argument('--output-path-prefix', '-o', default='pages-')
 	parser.add_argument('-v', '--verbose', action='store_true')
 	args = parser.parse_args()
-	namespaces = set(args.namespaces)
 
 	doc = xml.dom.pulldom.parse(args.input_path)
 	count = 0
-	with open(args.output_path, 'w', encoding='utf-8') as outFile:
-		outFile.write('<mediawiki>')
-		for event, pageNode in doc:
-			if event == xml.dom.pulldom.START_ELEMENT and pageNode.tagName == 'page':
-				doc.expandNode(pageNode)
-				ns = int(pulldomHelpers.getDescendantText(pageNode, 'ns'))
-				if ns in namespaces:
-					outFile.write(f'\n  {pageNode.toxml()}')
-				if args.verbose and count % VERBOSE_FACTOR == 0:
-					print(count)
-				count += 1
+	ns_files = {ns: open(f'{args.output_path_prefix}{ns}.xml', 'w', encoding='utf-8') for ns in args.namespaces}
+	for fi in ns_files.values():
+		fi.write('<mediawiki>')
 
-		outFile.write('\n</mediawiki>\n')
+	for event, pageNode in doc:
+		if event == xml.dom.pulldom.START_ELEMENT and pageNode.tagName == 'page':
+			doc.expandNode(pageNode)
+			actual_ns = int(pulldomHelpers.getDescendantText(pageNode, 'ns'))
+			try:
+				ns_files[actual_ns].write(f'\n  {pageNode.toxml()}')
+			except KeyError:
+				pass
+			if args.verbose and count % VERBOSE_FACTOR == 0:
+				print(count)
+			count += 1
+
+	for fi in ns_files.values():
+		fi.write('\n</mediawiki>\n')
+		fi.close()
 
 if __name__ == '__main__':
 	main()
