ed831a21d7d50f982b3966a7057c41f0f944ccba
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2024-04-27 13:47:54 -0700

    lang: Rewrite to use etree
    This gives an approx 5x speed improvement!
    See d1a5a64 (rewrite of ns) for details.

--- etree_helpers.py
+++ etree_helpers.py
@@ -4,10 +4,16 @@ import xml.etree.ElementTree as xet
 XML_NS_PATTERN = r'^\{.+?\}'
 
 def tag_without_xml_ns_is(elem: xet.Element, target_tag: str) -> bool:
-	return elem.tag.endswith(target_tag) and bool(re.fullmatch(XML_NS_PATTERN + re.escape(target_tag), elem.tag))
+	return elem.tag.endswith(target_tag) and bool(re.fullmatch(f'({XML_NS_PATTERN})?{re.escape(target_tag)}', elem.tag))
 
 def rm_xml_nses(elem: xet.Element) -> xet.Element:
 	elem.tag = re.sub(XML_NS_PATTERN, '', elem.tag)
 	for child in elem:
 		rm_xml_nses(child)
 	return elem
+
+def find_child(elem: xet.Element, tag: str) -> xet.Element | None:
+	try:
+		return next(child for child in elem if tag_without_xml_ns_is(child, tag))
+	except StopIteration:
+		return None
--- lang.py
+++ lang.py
@@ -1,14 +1,14 @@
 '''
-Filter only English terms out of an XML file.
+Filter terms in a specific language out of an XML file.
 '''
 
 import argparse
-import re
 import wikitextparser
-import xml.dom.minidom
-import xml.dom.pulldom
+import xml.etree.ElementTree as xet
 
-VERBOSE_FACTOR = 10 ** 4
+import etree_helpers
+
+VERBOSE_FACTOR = 10 ** 3
 
 def main():
 	parser = argparse.ArgumentParser()
@@ -18,38 +18,33 @@ def main():
 	parser.add_argument('-v', '--verbose', action='store_true', help='Prints occasional progress updates.')
 	args = parser.parse_args()
 
-	doc = xml.dom.pulldom.parse(args.input_path)
-	# only used for verbose printing
-	count = 0
 	with open(args.output_path, 'w', encoding='utf-8') as out_file:
 		out_file.write('<mediawiki>\n  ')
-		for event, node in doc:
-			if event == xml.dom.pulldom.START_ELEMENT and node.tagName == 'page':
-				page_node = node
-				doc.expandNode(page_node)
-				text_node = page_node.getElementsByTagName('text')[0]
-				text_node.normalize()
-				text = ''.join((node.data for node in text_node.childNodes))
+		# only used for verbose printing
+		count = 0
+		for _, elem in xet.iterparse(args.input_path):
+			if etree_helpers.tag_without_xml_ns_is(elem, 'page'):
+				text_elem = etree_helpers.find_child(etree_helpers.find_child(elem, 'revision'), 'text')
 				# perform a fast substring search first to avoid parsing most irrelevant pages
-				if args.language in text:
-					parsed = wikitextparser.parse(text)
+				if args.language in text_elem.text:
+					parsed = wikitextparser.parse(text_elem.text)
 					try:
 						target_section = next(section for section in parsed.get_sections(level=2) if section.title == args.language)
-						target_text = f'{parsed.get_sections(level=0)[0]}=={args.language}==\n{target_section.contents}'
-						target_node = xml.dom.minidom.Text()
-						target_node.replaceWholeText(target_text)
-						# a little hacky, and might not be officially supported, but works for now
-						text_node.childNodes = [target_node]
+						text_elem.text = f'{parsed.get_sections(level=0)[0]}=={args.language}==\n{target_section.contents}'
+						elem_xml = xet.tostring(elem, encoding='unicode')
 						out_file.write('  ')
-						page_node.writexml(out_file)
+						out_file.write(elem_xml)
 						out_file.write('\n')
 					# no section for target language
 					except StopIteration:
 						pass
+				elem.clear()
+
 				if args.verbose and count % VERBOSE_FACTOR == 0:
 					print(f'{count:,}')
 				count += 1
-		print('</mediawiki>', file=out_file)
+
+		out_file.write('</mediawiki>\n')
 
 if __name__ == '__main__':
 	main()
