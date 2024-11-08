0dcb23166f46c2979acccebdd1fa8674db26919c
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2024-11-08 00:44:10 -0800

    find_terms_lacking_prons: Add lowercase option
    Add an option to lowercase terms when looking up their frequencies.

--- find_terms_lacking_prons.py
+++ find_terms_lacking_prons.py
@@ -13,27 +13,31 @@ def main():
 	parser.add_argument('pages_path')
 	parser.add_argument('freqs_path')
 	parser.add_argument('output_path')
+	parser.add_argument('-l', '--lowercase', action='store_true', help='Lowercase terms when looking up their frequencies. Intended to be used in conjunction with the same option of find_frequencies.')
 	parser.add_argument('-v', '--verbose', action='store_true')
 	args = parser.parse_args()
 
 	with open(args.freqs_path, encoding='utf-8') as freq_file:
-		freqs = json.load(freq_file)
+		frequencies: dict[str, int] = json.load(freq_file)
+
+	def freq(term: str) -> int:
+		return freqs.get(page_title.casefold() if args.lowercase else page_title, 0)
 
 	terms_lacking_prons = []
 	for count, page in enumerate(etree_helpers.pages_gen(args.pages_path)):
 		page_title = etree_helpers.find_child(page, 'title').text
-		if page_title.isalnum() and freqs.get(page_title, 0) >= FREQUENCY_THRESHOLD:
+		if page_title.isalnum() and freq(page_title) >= FREQUENCY_THRESHOLD:
 			text = etree_helpers.find_child(etree_helpers.find_child(page, 'revision'), 'text').text
 			wikitext = wikitextparser.parse(text)
 			lang_section = next(sec for sec in wikitext.get_sections(level=2) if sec.title == 'English')
 			if not any(section.title == 'Pronunciation' and 3 <= section.level <= 5 for section in lang_section.sections):
-				terms_lacking_prons.append(page_title)
+				terms_lacking_prons.append(page_title.casefold() if args.lowercase else page_title)
 		page.clear()
 
 		if args.verbose and count % VERBOSE_FACTOR == 0:
 			print(f'{count:,}')
 
-	terms_lacking_prons.sort(key=lambda term: freqs.get(term, 0), reverse=True)
+	terms_lacking_prons.sort(key=freq, reverse=True)
 	with open(args.output_path, 'w', encoding='utf-8') as out_file:
 		for term in terms_lacking_prons:
 			print(term, file=out_file)
