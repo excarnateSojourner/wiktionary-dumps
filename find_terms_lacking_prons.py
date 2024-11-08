40e5bed4c369327366dfda0df5792f7af18b28e1
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2024-11-08 00:34:57 -0800

    find_terms_lacking_prons: Sort found terms by freq
    After common terms lacking pronunciations have been found, sort them
    by their frequency in Wikisource, with the most common words first.

--- find_terms_lacking_prons.py
+++ find_terms_lacking_prons.py
@@ -19,19 +19,24 @@ def main():
 	with open(args.freqs_path, encoding='utf-8') as freq_file:
 		freqs = json.load(freq_file)
 
-	with open(args.output_path, 'w', encoding='utf-8') as out_file:
-		for count, page in enumerate(etree_helpers.pages_gen(args.pages_path)):
-			page_title = etree_helpers.find_child(page, 'title').text
-			if page_title.isalnum() and not page_title.isupper() and not page_title.isnumeric() and freq(page_title) >= FREQUENCY_THRESHOLD:
-				text = etree_helpers.find_child(etree_helpers.find_child(page, 'revision'), 'text').text
-				wikitext = wikitextparser.parse(text)
-				lang_section = next(sec for sec in wikitext.get_sections(level=2) if sec.title == 'English')
-				if not any(section.title == 'Pronunciation' and 3 <= section.level <= 5 for section in lang_section.sections):
-					print(page_title, file=out_file)
-			page.clear()
+	terms_lacking_prons = []
+	for count, page in enumerate(etree_helpers.pages_gen(args.pages_path)):
+		page_title = etree_helpers.find_child(page, 'title').text
+		if page_title.isalnum() and freqs.get(page_title, 0) >= FREQUENCY_THRESHOLD:
+			text = etree_helpers.find_child(etree_helpers.find_child(page, 'revision'), 'text').text
+			wikitext = wikitextparser.parse(text)
+			lang_section = next(sec for sec in wikitext.get_sections(level=2) if sec.title == 'English')
+			if not any(section.title == 'Pronunciation' and 3 <= section.level <= 5 for section in lang_section.sections):
+				terms_lacking_prons.append(page_title)
+		page.clear()
 
-			if args.verbose and count % VERBOSE_FACTOR == 0:
-				print(f'{count:,}')
+		if args.verbose and count % VERBOSE_FACTOR == 0:
+			print(f'{count:,}')
+
+	terms_lacking_prons.sort(key=lambda term: freqs.get(term, 0), reverse=True)
+	with open(args.output_path, 'w', encoding='utf-8') as out_file:
+		for term in terms_lacking_prons:
+			print(term, file=out_file)
 
 if __name__ == '__main__':
 	main()
