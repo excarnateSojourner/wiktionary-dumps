72caaaca03640b03c993a710c6162ed9d39908e7
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2024-11-07 22:50:35 -0800

    find_frequencies: Take optional list of good IDs
    Add an option to find_frequencies to specify a file containing the IDs
    of pages which should be processed (with all other pages being
    skipped).
    Also use xml.etree.ElementTree.Element.clear() to remove each page
    from memory after it is processed.

--- find_frequencies.py
+++ find_frequencies.py
@@ -12,23 +12,40 @@ VALID_CHARS = string.ascii_letters + string.digits
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('pages_path')
-	parser.add_argument('output_path')
+	parser.add_argument('pages_path', help='The pages file to read words from. It is recommended that ns.py be used to get just pages in namespaces 0 and 114 (Translation).')
+	parser.add_argument('-g', '--ids_path', help='A text file containing page IDs, one per line, which should have their words counted. If given all other pages will be ignored.')
+	parser.add_argument('output_path', help='The JSON file in which to write the word counts.')
 	parser.add_argument('-v', '--verbose', action='store_true')
 	args = parser.parse_args()
 
-	frequencies = collections.defaultdict(int)
+	with open(args.ids_path, encoding='utf-8') as ids_file:
+		good_ids = [int(line) for line in ids_file]
+
+	frequencies = collections.Counter()
 	for count, page in enumerate(etree_helpers.pages_gen(args.pages_path)):
-		if count % VERBOSE_FACTOR == 0:
-			print(f'{count:,}')
-		text = etree_helpers.find_child(etree_helpers.find_child(page, 'revision'), 'text').text
-		if not text:
-			continue
-		text = wikitextparser.parse(text).plain_text()
-		for word in text.replace('-', ' ').split():
-			word = word.strip(punctuation)
-			if word and all(ch in VALID_CHARS for ch in word):
-				frequencies[word] += 1
+		try:
+			page_id = int(etree_helpers.find_child(page, 'id').text)
+			if page_id not in good_ids:
+				continue
+			raw_text = etree_helpers.find_child(etree_helpers.find_child(page, 'revision'), 'text').text
+			if not raw_text:
+				continue
+			try:
+				text = wikitextparser.parse(raw_text).plain_text()
+			# Raised by the 24-10-20 dump
+			except IndexError:
+				continue
+			valid_words = []
+			for word in text.replace('-', ' ').split():
+				word = word.strip(string.punctuation)
+				if word and all(ch in VALID_CHARS for ch in word):
+					valid_words.append(word)
+			frequencies.update(valid_words)
+		# Just to catch continues
+		finally:
+			page.clear()
+			if count % VERBOSE_FACTOR == 0:
+				print(f'{count:,}')
 
 	with open(args.output_path, 'w', encoding='utf-8') as out_file:
 		json.dump(frequencies, out_file, indent='\t')
