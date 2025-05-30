7772ee3c93f13485aea5073cffc6f1426b992d49
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2025-05-30 00:01:38 -0700

    song_parodier: Parse pronunciations, not rhymes
    Change song_parodier_prons to parse pronunciations, not rhymes. Also
    no longer care about syllable counts.

--- song_parodier_prons.py
+++ song_parodier_prons.py
@@ -1,6 +1,5 @@
 import argparse
 import collections
-import functools
 import json
 import re
 
@@ -11,40 +10,15 @@ import parsing.etree_helpers
 
 VERBOSITY_FACTOR = 10 ** 5
 # Long vowel indicators, syllable separators (normal ASCII period), and ties
-RHYME_CHARS_TO_REMOVE = str.maketrans('', '', '\u02D0.\u0361')
-GOOD_PARTS_OF_SPEECH = ['adjective', 'adverb', 'interjection', 'noun', 'verb']
-PARTS_OF_SPEECH = {
-	'adjective',
-	'adverb',
-	'ambiposition',
-	'article',
-	'circumposition',
-	'classifier',
-	'conjunction',
-	'contraction',
-	'counter',
-	'determiner',
-	'ideophone',
-	'interjection',
-	'noun',
-	'numeral',
-	'participle',
-	'particle',
-	'postposition',
-	'preposition',
-	'pronoun',
-	'proper noun',
-	'verb'
-}
-RHYME_TEMP_NAMES = ['rhymes', 'rhyme']
+PRON_CHARS_TO_REMOVE = str.maketrans('', '', '\u02D0.\u0361')
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('pages_path', help='Path of the XML file containing the text of each entry, in order to determine predominant parts of speech.')
+	parser.add_argument('pages_path', help='Path of the XML file containing the text of each entry, in order to find pronunciations.')
 	parser.add_argument('good_ids_path', help='Path of the file containing entry IDs (one per line) of terms considered acceptable replacements, as produced by find_terms.')
 	parser.add_argument('frequencies_path', help='Path of the JSON file containing word frequencies, as produced by find_frequencies.')
 	parser.add_argument('-l', '--language', default='English', help='The name of the language as it appears in the heading of each entry. Defaults to "English".')
-	parser.add_argument('output_path', help='Path of the file to write the rhyme category data to.')
+	parser.add_argument('output_path', help='Path of the file to write the pronunciation data to.')
 	parser.add_argument('-v', '--verbose', action='store_true')
 	args = parser.parse_args()
 
@@ -60,7 +34,7 @@ def main():
 
 	if args.verbose:
 		print('Reading entries:')
-	word_rhymes = collections.defaultdict(dict)
+	pron_data = collections.defaultdict(dict)
 	for page_count, page in enumerate(parsing.etree_helpers.pages_gen(args.pages_path)):
 		try:
 			page_id = int(parsing.etree_helpers.find_child(page, 'id').text)
@@ -72,39 +46,33 @@ def main():
 			wikitext = wikitextparser.parse(text)
 			try:
 				lang_sec = next(sec for sec in wikitext.get_sections(level=2) if sec.title == args.language)
+				pron_sec = next(sec for sec in lang_sec.sections if 3 <= sec.level <= 4 and sec.title == 'Pronunciation')
 			except StopIteration:
 				continue
 
-			# Find predominant part of speech
-			# If part of speech is not recognized this field is set to None, indicating the word is a function word
-			part_of_speech = next((sec.title.lower() for sec in lang_sec.sections if (sec.level == 3 or sec.level == 4) and sec.title.lower() in PARTS_OF_SPEECH), None)
-			word_rhymes[page_title]['part of speech'] = part_of_speech if part_of_speech in GOOD_PARTS_OF_SPEECH else None
-
-			# Find rhymes
-			rhymes: dict[int, list[str]] = collections.defaultdict(list)
-			for temp in lang_sec.templates:
-				if temp.normal_name() in RHYME_TEMP_NAMES:
-					# Skip over the first argument since it is the language code
-					temp_rhymes = [arg.value for arg in temp.arguments if arg.positional][1:]
-					for i, rhyme in enumerate(temp_rhymes, start=1):
-						syllable_count_arg = temp.get_arg(f's{i}') or temp.get_arg('s')
-						if syllable_count_arg:
-							rhyme = rhyme.translate(RHYME_CHARS_TO_REMOVE)
-							for syllable_count in syllable_count_arg.value.split(','):
-								# We could convert syllable_count to an int here, but there's no point since it will get converted back to a string in JSON
-								rhymes[syllable_count].append(rhyme)
-			if rhymes:
-				word_rhymes[page_title]['rhymes'] = rhymes
-			if page_id in good_ids and part_of_speech:
-				word_rhymes[page_title]['frequency'] = frequencies.get(page_title.casefold(), 0)
+			# Find pronunciations
+			prons: list[str] = []
+			for temp in pron_sec.templates:
+				if temp.normal_name() != 'IPA':
+					continue
+				# Skip over the first argument since it is the language code
+				temp_prons = [arg.value for arg in temp.arguments if arg.positional][1:]
+				for pron in temp_prons:
+					if pron.startswith('/') and pron.endswith('/'):
+						pron = pron[1:-1].translate(PRON_CHARS_TO_REMOVE)
+						prons.append(pron)
+			if prons:
+				pron_data[page_title]['pronunciations'] = prons
+			if page_id in good_ids:
+				pron_data[page_title]['frequency'] = frequencies.get(page_title.casefold(), 0)
 
 		finally:
 			if args.verbose and page_count % VERBOSITY_FACTOR == 0:
 				print(f'{page_count:,}')
 			page.clear()
 
-	with open(args.output_path, 'w', encoding='utf-8') as word_rhymes_file:
-		json.dump(word_rhymes, word_rhymes_file, indent='\t')
+	with open(args.output_path, 'w', encoding='utf-8') as pron_data_file:
+		json.dump(pron_data, pron_data_file, indent='\t')
 
 if __name__ == '__main__':
 	main()
