117ede966f3302ff9be65335367b9da60f2d4283
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2026-06-15 22:53:58 -0700

    undercategorized_temps: Use separated namespaces
    Update undercategorized_temps to expect parse_cats and parse_redirects to give separated namespace IDs.
    Previously it relied on namespace prefixes in the page titles.

--- task_lists/undercategorized_temps.py
+++ task_lists/undercategorized_temps.py
@@ -8,9 +8,9 @@ import parsing.parse_cats
 import parsing.parse_redirects
 import parsing.parse_stubs
 
+VERBOSE_FACTOR = 10 ** 6
 INSUFFICIENT_CATEGORIES = ['Templates and modules needing documentation']
-TEMPLATE_NS = 10
-TEMPLATE_PREFIX = 'Template:'
+TEMPLATE_NS_ID = 10
 
 def main():
 	parser = argparse.ArgumentParser()
@@ -18,19 +18,30 @@ def main():
 	parser.add_argument('cats_path', help='The path of the CSV file produced by parse_cats.')
 	parser.add_argument('redirects_path', help='The path of the CSV file produced by parse_redirects.')
 	parser.add_argument('output_path')
+	parser.add_argument('-v', '--verbose', action='store_true')
 	args = parser.parse_args()
 
-	temp_titles = {title.removeprefix(TEMPLATE_PREFIX) for id_, ns, title in parsing.parse_stubs.stubs_gen(args.stubs_path) if ns == TEMPLATE_NS and '/' not in title}
+	if args.verbose:
+		print('Forming initial template list...')
+	temp_titles = set()
+	for count, stub in enumerate(parsing.parse_stubs.stubs_gen(args.stubs_path)):
+		if args.verbose and count % VERBOSE_FACTOR == 0:
+			print(f'{count:,}')
+		if stub.ns == TEMPLATE_NS_ID and '/' not in stub.title:
+			temp_titles.add(stub.title)
 
-	for src_id, src_title, dst_id, dst_title in parsing.parse_redirects.redirects_gen(args.redirects_path):
-		if src_title.startswith(TEMPLATE_PREFIX):
-			temp_titles.discard(src_title.removeprefix(TEMPLATE_PREFIX))
+	if args.verbose:
+		print('Removing redirects...')
+	for src_id, src_ns, src_title, dst_id, dst_ns, dst_title in parsing.parse_redirects.redirects_gen(args.redirects_path):
+		if src_ns == TEMPLATE_NS_ID:
+			temp_titles.discard(src_title)
 
-	for cat_id, cat_title, page_id, page_title in parsing.parse_cats.cats_gen(args.cats_path):
-		if page_title.startswith(TEMPLATE_PREFIX):
-			temp_title = page_title.removeprefix(TEMPLATE_PREFIX)
-			if temp_title in temp_titles and cat_title not in INSUFFICIENT_CATEGORIES:
-				temp_titles.discard(temp_title)
+	if args.verbose:
+		print('Removing categorized templates...')
+	for cat_id, cat_title, page_id, page_ns, page_title in parsing.parse_cats.cats_gen(args.cats_path):
+		if page_ns == TEMPLATE_NS_ID:
+			if page_title in temp_titles and cat_title not in INSUFFICIENT_CATEGORIES:
+				temp_titles.discard(page_title)
 
 	with open(args.output_path, 'w', encoding='utf-8') as out_file:
 		for title in temp_titles:
